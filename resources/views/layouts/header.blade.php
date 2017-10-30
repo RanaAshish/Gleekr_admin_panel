41bfa6d87b63d97dcbcaa3f6d57fdfdf50af5ba7
sarfalesiddharth <sarfalesiddharth@gmail.com>
2017-10-30 10:05:05 -0700

    setup template

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -3,14 +3,14 @@
         <div class="navbar-header">
             <a href="javascript:void(0);" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar-collapse" aria-expanded="false"></a>
             <a href="javascript:void(0);" class="bars"></a>
-            <a class="navbar-brand" href="index.html">ADMINBSB - MATERIAL DESIGN</a>
+            <a class="navbar-brand" href="index.html">Gleekr</a>
         </div>
         <div class="collapse navbar-collapse" id="navbar-collapse">
-            <ul class="nav navbar-nav navbar-right">
-                <!-- Call Search -->
+<!--            <ul class="nav navbar-nav navbar-right">
+                 Call Search 
                 <li><a href="javascript:void(0);" class="js-search" data-close="true"><i class="material-icons">search</i></a></li>
-                <!-- #END# Call Search -->
-                <!-- Notifications -->
+                 #END# Call Search 
+                 Notifications 
                 <li class="dropdown">
                     <a href="javascript:void(0);" class="dropdown-toggle" data-toggle="dropdown" role="button">
                         <i class="material-icons">notifications</i>
@@ -118,8 +118,8 @@
                         </li>
                     </ul>
                 </li>
-                <!-- #END# Notifications -->
-                <!-- Tasks -->
+                 #END# Notifications 
+                 Tasks 
                 <li class="dropdown">
                     <a href="javascript:void(0);" class="dropdown-toggle" data-toggle="dropdown" role="button">
                         <i class="material-icons">flag</i>
@@ -196,9 +196,9 @@
                         </li>
                     </ul>
                 </li>
-                <!-- #END# Tasks -->
+                 #END# Tasks 
                 <li class="pull-right"><a href="javascript:void(0);" class="js-right-sidebar" data-close="true"><i class="material-icons">more_vert</i></a></li>
-            </ul>
+            </ul>-->
         </div>
     </div>
 </nav>
