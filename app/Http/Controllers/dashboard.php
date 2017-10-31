02e9cb09d8ca0d36db062c4023e1e589149e8e2a
Me\Mikinj <mikinj-mistry@hotmail.com>
2017-10-31 21:59:34 +0530

    Create new project to solve issue

---
+++ app/Http/Controllers/dashboard.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class dashboard extends Controller
+{
+    	
+    public function index(){
+        die("here");
+        return view('dashboard');
+    }
+
+}
