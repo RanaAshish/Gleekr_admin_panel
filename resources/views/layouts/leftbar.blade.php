7a22e7779fa6545840a107dc763cc5667a47d34e
Me\Mikinj <mikinj-mistry@hotmail.com>
2017-11-12 20:24:40 +0530

    Changes

--- resources/views/layouts/leftbar.blade.php
+++ resources/views/layouts/leftbar.blade.php
@@ -7,14 +7,14 @@
                 <img src="images/user.png" width="48" height="48" alt="User" />
             </div>
             <div class="info-container">
-                <div class="name" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">John Doe</div>
-                <div class="email">john.doe@example.com</div>
-                <div class="btn-group user-helper-dropdown">
+                <div class="name" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Gleekr Admin</div>
+                <div class="email"></div>
+<!--                <div class="btn-group user-helper-dropdown">
                     <i class="material-icons" data-toggle="dropdown" aria-haspopup="true" aria-expanded="true">keyboard_arrow_down</i>
                     <ul class="dropdown-menu pull-right">
                         <li><a href="logout"><i class="material-icons">input</i>Sign Out</a></li>
                     </ul>
-                </div>
+                </div>-->
             </div>
         </div>
         <!-- #User Info -->
